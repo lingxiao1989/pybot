505844ffc4cebaa96c6dfd1f4494cc9e582b95d5
Xiao <lingxiao1989@gmail.com>
2019-11-10 15:59:55 +0800

    Add files via upload

---
+++ program.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from library import *
+'''
+Program Your Robot. Complete The Task.
+Write Your Program Below
+'''
+a = motor('A')
+b = motor("B")
+run([a,b],[1050,1050],100)
+
+
+'''
+Write Your Program Above
+Your Program is Awesome :)
+'''
+while True:
+    pass
